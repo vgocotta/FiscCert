f9cb7f7bc1b166a29f27d056e1924a424908c8e8
Vitor Cotta <vgoc2@hotmail.com>
2026-02-18 14:55:05 -0300

    test: add unit tests for Certificate entity validation logic

---
+++ tests/FiscCert.UnitTest/CertificateTests.cs
@@ -0,0 +1,53 @@
+﻿using FiscCert.Domain;
+
+namespace FiscCert.UnitTest;
+
+public class CertificateTests
+{
+
+    [Fact]
+    public void IsValid_ShouldReturnTrue_WhenDateIsFutureAndNotRevoked()
+    {
+        // Arrange
+        DateTime referenceDate = new(2026, 1, 1);
+        DateTime expirationDate = new(2026, 1, 2);
+        Certificate certificate = new("Test Owner", "12345678000190", expirationDate);
+
+        // Act
+        bool isValid = certificate.IsValid(referenceDate);
+
+        // Assert
+        Assert.True(isValid);
+    }
+
+    [Fact]
+    public void IsValid_ShouldReturnFalse_WhenDateIsPast()
+    {
+        // Arrange
+        DateTime referenceDate = new(2026, 1, 3);
+        DateTime expirationDate = new(2026, 1, 2);
+        Certificate certificate = new("Test Owner", "12345678000190", expirationDate);
+     
+        // Act
+        bool isValid = certificate.IsValid(referenceDate);
+        
+        // Assert
+        Assert.False(isValid);
+    }
+
+    [Fact]
+    public void IsValid_ShouldReturnFalse_WhenCertificateIsRevoked()
+    {
+        // Arrange
+        DateTime referenceDate = new(2026, 1, 1);
+        DateTime expirationDate = new(2026, 1, 2);
+        Certificate certificate = new("Test Owner", "12345678000190", expirationDate);
+        certificate.Revoke();
+     
+        // Act
+        bool isValid = certificate.IsValid(referenceDate);
+        
+        // Assert
+        Assert.False(isValid);
+    }
+}
